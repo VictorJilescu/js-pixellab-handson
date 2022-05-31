a1782eb40b87218c835abb7e8c02c3c0406a026a
VictorJilescu <97190875+VictorJilescu@users.noreply.github.com>
2022-05-31 22:00:56 +0300

    S04

---
+++ M01S04/ex04/app.js
@@ -0,0 +1,29 @@
+var i = 0;
+
+// for (i = 0; i <= 3; i++) {
+//   console.log(i);
+// }
+
+// for (i = 0; i <= 3; i++) {
+//   console.log(i);
+// }
+// for (i = 98; i < 100; i++) {
+//   console.log(i);
+// }
+
+// for (i = 1; i < 100; i++) {
+//   console.log(i);
+// }
+
+// for (i = 1; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+for (i = 1; i <= 100; i++) {
+  if (i % 5 === 0) {
+    console.log();
+  }
+}
